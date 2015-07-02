6baae4056c7831c609d641bf6bef1f8b63f44f4c
Chris Archibald <carchi8py@gmail.com>
2015-07-02 15:10:00 -0700

    Made change to view will appear to run only when we have a moive

--- MyFavoriteMovies/MovieDetailViewController.swift
+++ MyFavoriteMovies/MovieDetailViewController.swift
@@ -37,98 +37,103 @@ class MovieDetailViewController: UIViewController {
         
         super.viewWillAppear(animated)
         
-        /* TASK A: Get favorite movies, then update the favorite buttons */
-        /* 1A. Set the parameters */
-        let methodParameters = [
-            "api_key": appDelegate.apiKey,
-            "session_id": appDelegate.sessionID!
-        ]
+        if let movie = movie {
+            titleLabel.text = movie.title
+            unFavoriteButton.hidden = true
         
-        /* 2A. Build the URL */
-        let urlString = appDelegate.baseURLSecureString + "account/\(appDelegate.userID!)/favorite/movies" + appDelegate.escapedParameters(methodParameters)
-        let url = NSURL(string: urlString)!
+            /* TASK A: Get favorite movies, then update the favorite buttons */
+            /* 1A. Set the parameters */
+            let methodParameters = [
+                "api_key": appDelegate.apiKey,
+                "session_id": appDelegate.sessionID!
+            ]
+            
+            /* 2A. Build the URL */
+            let urlString = appDelegate.baseURLSecureString + "account/\(appDelegate.userID!)/favorite/movies" + appDelegate.escapedParameters(methodParameters)
+            let url = NSURL(string: urlString)!
 
-        /* 3A. Configure the request */
-        let request = NSMutableURLRequest(URL: url)
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        /* 4A. Make the request */
-        let task = session.dataTaskWithRequest(request) { data, responce, downloadError
-            in
-            if let error = downloadError {
-                println("could not complete request \(error)")
-            } else {
-                /* 5A. Parse the data */
-                var parsingError: NSError? = nil
-                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options:
-                    NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
-                
-                /* 6A. Use the data! */
-                if let error = parsingError {
-                    println(error)
-                } else {
-                    if let results = parsedResult["results"] as? [[String: AnyObject]] {
-                        var isFavorite = false
-                        let moives = Movie.moviesFromResults(results)
-                        
-                        for movie in moives {
-                            if movie.id == self.movie!.id {
-                                isFavorite = true
-                            }
-                        }
-                        
-                        dispatch_async(dispatch_get_main_queue()) {
-                            if isFavorite {
-                                self.favoriteButton.hidden = true
-                                self.unFavoriteButton.hidden = false
-                            } else {
-                                self.favoriteButton.hidden = false
-                                self.unFavoriteButton.hidden = true
-                            }
-                        }
-                    } else {
-                        println("Couldn't find results in \(parsedResult)")
-                    }
-                }
-            }
-        }
-         /* 7A. Start the request */
-        task.resume()
-        
-        /* TASK B: Get the poster image, then populate the image view */
-        if let posterPath = movie!.posterPath {
+            /* 3A. Configure the request */
+            let request = NSMutableURLRequest(URL: url)
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
             
-            /* 1B. Set the parameters */
-            // There are none...
-            
-            /* 2B. Build the URL */
-            let baseURL = NSURL(string: appDelegate.config.baseImageURLString)!
-            let url = baseURL.URLByAppendingPathComponent("w342").URLByAppendingPathComponent(posterPath)
-            
-            /* 3B. Configure the request */
-            let request = NSURLRequest(URL: url)
-            
-            /* 4B. Make the request */
-            let task = session.dataTaskWithRequest(request) {data, response, downloadError in
-                
+            /* 4A. Make the request */
+            let task = session.dataTaskWithRequest(request) { data, responce, downloadError
+                in
                 if let error = downloadError {
-                    println(error)
+                    println("could not complete request \(error)")
                 } else {
+                    /* 5A. Parse the data */
+                    var parsingError: NSError? = nil
+                    let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options:
+                        NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
                     
-                    /* 5B. Parse the data */
-                    // No need, the data is already raw image data.
-                    
-                    /* 6B. Use the data! */
-                    if let image = UIImage(data: data!) {
-                        dispatch_async(dispatch_get_main_queue()) {
-                            self.posterImageView!.image = image
+                    /* 6A. Use the data! */
+                    if let error = parsingError {
+                        println(error)
+                    } else {
+                        if let results = parsedResult["results"] as? [[String: AnyObject]] {
+                            var isFavorite = false
+                            let moives = Movie.moviesFromResults(results)
+                            
+                            for movie in moives {
+                                if movie.id == self.movie!.id {
+                                    isFavorite = true
+                                }
+                            }
+                            
+                            dispatch_async(dispatch_get_main_queue()) {
+                                if isFavorite {
+                                    self.favoriteButton.hidden = true
+                                    self.unFavoriteButton.hidden = false
+                                } else {
+                                    self.favoriteButton.hidden = false
+                                    self.unFavoriteButton.hidden = true
+                                }
+                            }
+                        } else {
+                            println("Couldn't find results in \(parsedResult)")
                         }
                     }
                 }
             }
-        
-            /* 7B. Start the request */
+             /* 7A. Start the request */
             task.resume()
+            
+            /* TASK B: Get the poster image, then populate the image view */
+            if let posterPath = movie.posterPath {
+                
+                /* 1B. Set the parameters */
+                // There are none...
+                
+                /* 2B. Build the URL */
+                let baseURL = NSURL(string: appDelegate.config.baseImageURLString)!
+                let url = baseURL.URLByAppendingPathComponent("w342").URLByAppendingPathComponent(posterPath)
+                
+                /* 3B. Configure the request */
+                let request = NSURLRequest(URL: url)
+                
+                /* 4B. Make the request */
+                let task = session.dataTaskWithRequest(request) {data, response, downloadError in
+                    
+                    if let error = downloadError {
+                        println(error)
+                    } else {
+                        
+                        /* 5B. Parse the data */
+                        // No need, the data is already raw image data.
+                        
+                        /* 6B. Use the data! */
+                        if let image = UIImage(data: data!) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self.posterImageView!.image = image
+                            }
+                        }
+                    }
+                }
+            
+                /* 7B. Start the request */
+                task.resume()
+            }
         }
     }
     
